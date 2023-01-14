5b6fa9bcd42f0991e5b553209df201efa855d68f
Takumi Aoyama <aotaku4062@gmail.com>
2023-01-14 23:51:41 +0900

    新規プロジェクトの追加 新規レベルの追加

---
+++ ObstacleGame/Intermediate/Build/Win64/ObstacleGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31630
+#include "C:/Users/aotak/OneDrive/デスクトップ/Unreal_ObstacleGame/ObstacleGame/Intermediate/Build/Win64/ObstacleGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ObstacleGame/Intermediate/Build/Win64/UnrealEditor/Inc/ObstacleGame/ObstacleGame.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeObstacleGame_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_ObstacleGame;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_ObstacleGame()
+	{
+		if (!Z_Registration_Info_UPackage__Script_ObstacleGame.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/ObstacleGame",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x9533B1A9,
+				0x7360B108,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ObstacleGame.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_ObstacleGame.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ObstacleGame(Z_Construct_UPackage__Script_ObstacleGame, TEXT("/Script/ObstacleGame"), Z_Registration_Info_UPackage__Script_ObstacleGame, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x9533B1A9, 0x7360B108));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
